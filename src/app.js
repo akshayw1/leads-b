10679698eb2eaf32136a6fbfa21a0c9da775b81a
leandre <tuyambazeleandre@gmail.com>
2023-12-20 18:37:17 +0200

    fix cors issues

--- src/app.js
+++ src/app.js
@@ -12,6 +12,18 @@ import { resetAccess } from './cron-job/reset.access';
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 
 app.use(morgan('combined'));
 app.use(express.json());
